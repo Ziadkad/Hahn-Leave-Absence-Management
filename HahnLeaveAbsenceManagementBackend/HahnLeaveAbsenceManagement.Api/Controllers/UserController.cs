74b6833275b8dc892f38d8def1a0e69e15f0757a
Ziad KADDARI <ziadkaddari1@gmail.com>
2025-08-27 14:17:36 +0100

    Adding addLeaves Api

--- HahnLeaveAbsenceManagementBackend/HahnLeaveAbsenceManagement.Api/Controllers/UserController.cs
+++ HahnLeaveAbsenceManagementBackend/HahnLeaveAbsenceManagement.Api/Controllers/UserController.cs
@@ -1,4 +1,7 @@
-﻿using HahnLeaveAbsenceManagement.Application.User.Queries;
+﻿using HahnLeaveAbsenceManagement.Api.Security;
+using HahnLeaveAbsenceManagement.Application.Common.Exceptions;
+using HahnLeaveAbsenceManagement.Application.User.Commands;
+using HahnLeaveAbsenceManagement.Application.User.Queries;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -13,5 +16,16 @@ public class UserController : BaseController
         var result = await Mediator.Send(new GetAllUsersQuery());
         return Ok(result);
     }
-
+    
+    [Authorize(AuthPolicyName.HumanResourcesManager)]
+    [HttpPut("AddLeaves/{userId}")]
+    public async Task<IActionResult> AddLeaves(Guid userId, AddLeavesCommand command)
+    {
+        if (userId != command.UserId)
+        {
+            throw new BadRequestException("Ids do not match");
+        }
+        var result = await Mediator.Send(command);
+        return Ok(result);
+    }
 }
---
+++ HahnLeaveAbsenceManagementBackend/HahnLeaveAbsenceManagement.Application/User/Commands/AddLeavesCommand.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using FluentValidation;
+using HahnLeaveAbsenceManagement.Application.Common.Exceptions;
+using HahnLeaveAbsenceManagement.Application.Common.Interfaces;
+using HahnLeaveAbsenceManagement.Application.User.Models;
+using MediatR;
+
+namespace HahnLeaveAbsenceManagement.Application.User.Commands;
+
+public record AddLeavesCommand(Guid UserId, int Days) : IRequest<UserDto>;
+
+public class AddLeavesCommandValidator : AbstractValidator<AddLeavesCommand>
+{
+    public AddLeavesCommandValidator()
+    {
+        RuleFor(x => x.UserId).NotEmpty().WithMessage("UserId should not be empty");
+        RuleFor(x => x.Days)
+            .NotEmpty().WithMessage("Days should not be empty")
+            .GreaterThan(0).WithMessage("Days must be greater than zero");
+    }
+}
+
+public class AddLeavesCommandHandler(IUserRepository userRepository, IUnitOfWork unitOfWork, IMapper mapper) : IRequestHandler<AddLeavesCommand, UserDto>
+{
+    public async Task<UserDto> Handle(AddLeavesCommand request, CancellationToken cancellationToken)
+    {
+        Domain.User.User? user = await userRepository.FindAsync(request.UserId,cancellationToken);
+        if (user == null)
+        {
+            throw new NotFoundException(nameof(user), request.UserId);
+        }
+         user.AddLeaveDays(request.Days);
+         var isSaved = await unitOfWork.SaveChangesAsync(cancellationToken);
+         if (isSaved <= 0)
+         {
+             throw new InternalServerException();
+         }
+        
+         return mapper.Map<UserDto>(user);
+    }
+}
