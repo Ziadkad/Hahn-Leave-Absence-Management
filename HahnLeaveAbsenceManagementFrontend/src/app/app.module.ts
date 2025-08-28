68c05ce35a4bfc6aff9fdca4b1d369dd0b195986
Ziad KADDARI <ziadkaddari1@gmail.com>
2025-08-28 11:05:53 +0100

    Update

--- HahnLeaveAbsenceManagementFrontend/src/app/app.module.ts
+++ HahnLeaveAbsenceManagementFrontend/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './layout/sidebar/sidebar.component';
+import { LoginPageComponent } from './features/auth/pages/login-page/login-page.component';
+import { RegisterPageComponent } from './features/auth/pages/register-page/register-page.component';
+import { RegisterFormComponent } from './features/auth/components/register-form/register-form.component';
+import { LoginFormComponent } from './features/auth/components/login-form/login-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    LoginPageComponent,
+    RegisterPageComponent,
+    RegisterFormComponent,
+    LoginFormComponent
   ],
   imports: [
     BrowserModule,
