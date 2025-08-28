14e0ec2ccde1149104e34c6e4dad79e8a278a995
Ziad KADDARI <ziadkaddari1@gmail.com>
2025-08-28 19:51:09 +0100

    Enhance sidebar component with user authentication and profile display

--- HahnLeaveAbsenceManagementFrontend/src/app/layout/sidebar/sidebar.component.ts
+++ HahnLeaveAbsenceManagementFrontend/src/app/layout/sidebar/sidebar.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import {Component, effect} from '@angular/core';
+import {AuthService} from "../../core/services/auth-service/auth.service";
+import {Router} from "@angular/router";
+import {UserRole} from "../../core/interfaces/user-interfaces/user-Role";
+import {LoginResponse} from "../../core/interfaces/auth-interfaces/login-response";
+import {ToastrService} from "ngx-toastr";
 
 @Component({
   selector: 'app-sidebar',
@@ -6,5 +11,22 @@ import { Component } from '@angular/core';
   styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent {
+  isAuthenticated = this.authService.isAuthenticated();
+  userRole: UserRole | undefined;
+  currentUser: LoginResponse | null = null;
+  constructor(private readonly authService: AuthService,
+              private readonly router: Router,
+              private readonly toastr: ToastrService,) {
+    effect(()=>{
+      this.isAuthenticated = this.authService.isAuthenticated();
+      this.currentUser = this.authService.currentUser();
+      this.userRole = this.currentUser?.role;
+    })
+  }
 
+  logout() {
+    this.authService.logout();
+    this.toastr.success('Logged out');
+    this.router.navigate(['']);
+  }
 }
